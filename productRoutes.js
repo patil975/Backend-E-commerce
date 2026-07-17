57444ac3c4c171b10146a287b44b8842b3679708
patil975 <patilrachana497@gmail.com>
2026-07-17 15:08:21 +0530

    Add files via upload

---
+++ productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { addProduct, getProducts, deleteProduct, updateProducts } = require('../controllers/productController')
+const authMiddleware = require('../middleware/authMiddleware')
+const adminMiddleware = require('../middleware/adminMiddleware')
+
+
+const router = express.Router()
+
+router.post('/addproduct',authMiddleware,adminMiddleware,addProduct)
+router.get('/getproduct',getProducts)
+router.delete('/deleteproduct/:id',deleteProduct)
+router.put('/updateproduct/:id',updateProducts)
+
+module.exports = router
